761794d57a95b5f7b08fd29954dce8ffb40f798e
MaximPozdnyakov <maximpozdnyakow@gmail.com>
2021-05-21 16:20:46 +0300

    Set up express.js

---
+++ server/app.js
@@ -0,0 +1,14 @@
+const express = require("express");
+
+const app = express();
+
+// Body parser
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+// Routes
+app.use("/api/pdf", require("./routes/pdf"));
+
+const PORT = 3000;
+
+app.listen(PORT, console.log(`Server running on port ${PORT}`));
