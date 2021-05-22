326cdd2f34f88f8114d5cfd3ed208f21c35459d6
MaximPozdnyakov <maximpozdnyakow@gmail.com>
2021-05-22 10:42:13 +0300

    Add test data for pdf generation. Create a template that will contain all pages. Configure pdf generation, fix display error

--- server/routes/pdf.js
+++ server/routes/pdf.js
@@ -1,8 +1,29 @@
 const express = require("express");
 const router = express.Router();
+const fs = require("fs");
+const html = fs.readFileSync("dist/pdf-full.html", "utf8");
+const css = fs.readFileSync("dist/css/style.min.css", "utf8");
+const pdf = require("html-pdf");
+const pdfOptions = { format: "A4" };
+const { parse } = require("node-html-parser");
 
 router.get("/", (req, res) => {
-  res.json({ message: "yeeee" });
+  const parsedHtml = parse(html);
+  parsedHtml
+    .querySelector("head")
+    .insertAdjacentHTML("beforeend", `<style>${css}</style>`);
+
+  pdf
+    .create(parsedHtml.toString(), pdfOptions)
+    .toStream(function (err, stream) {
+      if (err) return console.log(err);
+      res.setHeader("Content-Type", "application/pdf");
+      res.setHeader(
+        "Content-Disposition",
+        "attachment; filename=SupplierProfile.pdf"
+      );
+      stream.pipe(res);
+    });
 });
 
 module.exports = router;
